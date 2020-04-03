983bb60574b6d1d452e3284fac40137a5aa4d4fb
yaoe <emmanuel.yao@grenoble-inp.org>
2020-04-03 22:40:26 +0200

    Ajout d'un main et mise en forme de base de la fenetre

--- vue/Fenetre.java
+++ vue/Fenetre.java
@@ -5,9 +5,22 @@ import javax.swing.JFrame;
 import equation.Operation;
 
 public class Fenetre extends JFrame {
+	
+	public static final int longueur = 800;
+	public static final int largeur =700;
 
 	public Fenetre() {
-		// TODO Auto-generated constructor stub
+
+		  // Parametre de la fenetre
+		  this.setTitle("Calcul Mental");
+		  this.setSize(longueur, largeur);
+		  this.setLocationRelativeTo(null);
+		  this.setResizable(false);
+		  //this.setContentPane(pan);
+		  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+
+		  // Fenetre Visible
+		  this.setVisible(true);
 	}
 	
 	public void update(boolean verif, Operation new_o) {
---
+++ vue/Main.java
@@ -0,0 +1,10 @@
+package vue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Fenetre();
+
+	}
+
+}
